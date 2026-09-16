1af08831020cf842b01755b3f120f5e8f450138f
applelover <hera@naver.com>
2026-09-16 10:25:30 +0900

    aws push3

--- springboot-app2/src/main/java/com/example/demo/UserController2.java
+++ springboot-app2/src/main/java/com/example/demo/UserController2.java
@@ -16,6 +16,7 @@ public class UserController2 {
     public String hello() {
         return "<body bgcolor=yellow>changed!!!! -------</body>";
     }
+    
 
     @GetMapping("/mysql")
     public String dbTest() {
